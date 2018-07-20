4254b4f055e2bb0cb0f35066d9a08ac1f6db9e81
Susanna Kline <susannakline@gmail.com>
2018-07-20 15:13:21 -0500

    Add event listener for card clicks

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+/* * card open show
+ * card
+ * card match
+ * --
+ * fa fa-diamond
+ * fa fa-paper-plane-o
+ * fa fa-anchor
+ * fa fa-bolt
+ * fa fa-bomb
+ * fa fa-leaf
+ * fa fa-bicycle
+ * fa fa-cube
+ */
+
 /*
  * Create a list that holds all of your cards
  */
@@ -36,3 +50,11 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+const memoryCards = document.querySelectorAll('.card');
+
+for (memoryCard of memoryCards) {
+    memoryCard.addEventListener('click', function(){
+        /* display card's symbol*/
+        /* add card to a list of open cards*/
+     })
+}
