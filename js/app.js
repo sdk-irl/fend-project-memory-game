b2413561fdb131f5b2ec3789848bd3492aa97d09
Susanna Kline <susannakline@gmail.com>
2018-07-29 14:21:32 -0500

    Implement shuffle function and add comments throughout

--- js/app.js
+++ js/app.js
@@ -1,31 +1,20 @@
-/* * card open show
- * card
- * card match
- * --
- * fa fa-diamond
- * fa fa-paper-plane-o
- * fa fa-anchor
- * fa fa-bolt
- * fa fa-bomb
- * fa fa-leaf
- * fa fa-bicycle
- * fa fa-cube
- */
 
-/*
- * Create a list that holds all of your cards to pass into the shuffle function
- */
-/*let allCards = [ 
-    ""
-]
-array.from() turns node list into an array
-*/
 let toggledCardsArray = [];
+const memoryCardDeck = document.querySelector('.deck');
 
+/* List that holds all of your cards to pass into the shuffle function */
+function shuffleCards() {
+    const cards = document.querySelectorAll('.deck li');
+    let cardArray = Array.from(cards);
+    let shuffledCards = shuffle(cardArray);
+    for (card of shuffledCards) {
+        memoryCardDeck.appendChild(card);
+    }
+}
+shuffleCards();
 
 /*
  * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
@@ -45,31 +34,33 @@ function shuffle(array) {
     return array;
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
+
+/* * If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    = + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    = + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-const memoryCardDeck = document.querySelector('.deck');
-
 /* display card's symbol*/
 
+/* Set up the event listener for a card */
 memoryCardDeck.addEventListener('click', function(){
     const evtTgt = event.target; 
+    /* If cards meet criteria, toggle them to open, add to list */
     if (checkClick(evtTgt)) {
         toggleCard(evtTgt);
         addCardToList(evtTgt);
+        /* When two cards in array, check for matching set */
         if (toggledCardsArray.length === 2) {
             checkCardMatch();
         }
     }
 });
 
+/* Function to check card for match, for less than 2 cards, and not the same card */
 function checkClick(evtTgt) {
     return (
         (evtTgt.classList.contains('card')
@@ -79,15 +70,18 @@ function checkClick(evtTgt) {
         )
     )}
 
+/* Function to toggle cards to open */
 function toggleCard(evtTgt){
     evtTgt.classList.toggle('open');
     evtTgt.classList.toggle('show');
 }
 
+/* Function to add cards to the card deck */
 function addCardToList(evtTgt) {
     toggledCardsArray.push(evtTgt);
 } 
 
+/* Function to see if cards match */
 function checkCardMatch() {
     if (toggledCardsArray[0].firstElementChild.className 
     /* if toggled cards match, change to match color, toggle class to match, and empty array */
@@ -102,7 +96,7 @@ function checkCardMatch() {
       */
     }
     else {
-    /* wait a couple seconds for player to see cards, then toggles cards, then resets card array; */
+    /* Wait a couple seconds for player to see cards, then toggles cards, then resets card array; */
        setTimeout(function timeoutID() {
             toggleCard(toggledCardsArray[0]);
             toggleCard(toggledCardsArray[1]);
